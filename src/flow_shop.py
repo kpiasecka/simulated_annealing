71e5e4ecabd285fede874b3e8308c365a81b51df
Kalina Piasecka <kalinapiasecka9@gmail.com>
2023-03-28 20:47:21 +0200

    refactor(flow-shop): problem instance

--- src/flow_shop.py
+++ src/flow_shop.py
@@ -2,18 +2,17 @@ from __future__ import annotations
 
 from random import randint
 from typing import List, Optional
-
-Times = List[List[int]]
+import numpy as np
 
 
 class ProblemInstance:
-    def __init__(self, machines: int, tasks: int, times: Optional[Times] = None):
+    def __init__(self, machines: int, tasks: int, times: Optional[np.array] = None):
         self.machines = machines
         self.tasks = tasks
-        self._times = times if not None else [[]]
+        self._times = times if not None else np.empty((machines, tasks))
 
     @property
-    def times(self) -> Times:
+    def times(self) -> np.array:
         return self._times
 
     @staticmethod
