bdf4098baf55750c7a654abd351adfc32b57b1c7
Kalina Piasecka <kalinapiasecka9@gmail.com>
2023-03-28 21:10:17 +0200

    refactor: finishing touches

--- src/flow_shop.py
+++ src/flow_shop.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
 from random import randint
-from typing import List, Optional
 import numpy as np
 
 
 class ProblemInstance:
-    def __init__(self, machines: int, tasks: int, times: Optional[np.array] = None):
-        self.machines = machines
-        self.tasks = tasks
-        self._times = times if not None else np.empty((machines, tasks))
+    def __init__(self, times: list):
+        self._times = np.array(times)
+
+    @property
+    def machines(self) -> int:
+        return self._times.shape[0]
+
+    @property
+    def tasks(self) -> int:
+        return self._times.shape[1]
 
     @property
     def times(self) -> np.array:
@@ -19,4 +24,4 @@ class ProblemInstance:
     def generate(tasks: int, machines: int) -> ProblemInstance:
         times = [[randint(1, 99) for _ in range(tasks)] for __ in range(machines)]
 
-        return ProblemInstance(machines, tasks, times)
+        return ProblemInstance(times)
