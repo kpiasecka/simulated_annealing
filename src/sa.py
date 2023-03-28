c1445d9bceb21717e52f77d83105390abdc3480c
Kalina Piasecka <kalinapiasecka9@gmail.com>
2023-03-28 19:53:19 +0200

    feat: sa backbone

--- src/sa.py
+++ src/sa.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
+
+import math
 from abc import ABC, abstractmethod
-from typing import List
-
-
-ProblemInstance = List[List[int]]
+from flow_shop import ProblemInstance
+import numpy as np
+from random import choice
 
 
 class StopCondition(ABC):
@@ -15,7 +16,7 @@ class StopCondition(ABC):
 class IterativeCondition(StopCondition):
     def __init__(self, n: int):
         self.n = n
-        self.i = 0   # iteration
+        self.i = 0  # iteration
 
     def should_stop(self) -> bool:
         self.i += 1
@@ -47,7 +48,7 @@ class Cooling(ABC):
 
 
 class GeometricCooling(Cooling):
-    def __int__(self, alpha: float):
+    def __init__(self, alpha: float):
         self.alpha = alpha
 
     def cool_down(self, temp: float) -> float:
@@ -55,7 +56,7 @@ class GeometricCooling(Cooling):
 
 
 class LinearCooling(Cooling):
-    def __int__(self, beta: float):
+    def __init__(self, beta: float):
         self.beta = beta
 
     def cool_down(self, temp: float) -> float:
@@ -63,12 +64,40 @@ class LinearCooling(Cooling):
 
 
 class SA:
-    def __int__(self, temp: float, cooling: Cooling, stop_condition: StopCondition, criteria: Criteria):
+    def __init__(self, temp: float, cooling: Cooling, stop_condition: StopCondition, criteria: Criteria):
         self.temp = temp
         self.cooling = cooling
         self.stop_condition = stop_condition
         self.criteria = criteria
+        self.best_solution = np.array([])
 
-    def solve(self, problem_instance: ProblemInstance):
-        pass
+    def solve(self, problem_instance: ProblemInstance) -> np.array:
+        self.get_random_solution(problem_instance.tasks)
+        while not self.stop_condition.should_stop():
+            next_solution = self.move(self.best_solution)
+            if self.should_accept(next_solution):
+                self.best_solution = next_solution
+            self.temp = self.cooling.cool_down(self.temp)
+        return self.best_solution
 
+    def get_random_solution(self, tasks: int) -> None:
+        self.best_solution = np.random.permutation(tasks)
+
+    @staticmethod
+    def move(best_solution: np.array) -> np.array:
+        first_task = np.random.randint(0, len(best_solution))
+        left_tasks = [i for i in range(len(best_solution)) if i != first_task]
+        second_task = choice(left_tasks)
+        new_solution = best_solution.copy()
+        new_solution[first_task], new_solution[second_task] = new_solution[second_task], new_solution[first_task]
+        return new_solution
+
+    def should_accept(self, next_solution: np.array) -> bool:
+        best_solution_value = self.criteria.get_value(self.best_solution)
+        next_solution_value = self.criteria.get_value(next_solution)
+
+        if next_solution_value < best_solution_value:
+            return True  # its better
+        else:
+            probability = math.exp(-(next_solution_value - best_solution_value) / self.temp)
+            return np.random.rand() <= probability
