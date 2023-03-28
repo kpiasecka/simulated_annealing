b9be0b0d06e8ca20f84b04991c9c6f0ec743117b
Kalina Piasecka <kalinapiasecka9@gmail.com>
2023-03-28 19:19:11 +0200

    refactor: flow shop

--- src/flow_shop.py
+++ src/flow_shop.py
@@ -1,6 +1,23 @@
+from __future__ import annotations
+
 from random import randint
+from typing import List, Optional
+
+Times = List[List[int]]
 
 
-def generate_instance(machines_no: int, tasks_no: int):
-    return [[randint(1, 99) for _ in range(tasks_no)] for __ in range(machines_no)]
+class ProblemInstance:
+    def __init__(self, machines: int, tasks: int, times: Optional[Times] = None):
+        self.machines = machines
+        self.tasks = tasks
+        self._times = times if not None else [[]]
 
+    @property
+    def times(self) -> Times:
+        return self._times
+
+    @staticmethod
+    def generate(tasks: int, machines: int) -> ProblemInstance:
+        times = [[randint(1, 99) for _ in range(tasks)] for __ in range(machines)]
+
+        return ProblemInstance(machines, tasks, times)
